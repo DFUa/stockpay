b0bcc764af9d36bd647bc9478d7b7166ae3d2f27
Just Programmer <polishchuk256@gmail.com>
2019-02-04 10:53:08 +0200

    Add UiToggleArrow..

---
+++ src/components/ui/ui-toggle-arrow/UiToggleArrow.vue
@@ -0,0 +1,53 @@
+<template>
+  <div @click="toggle" class="ui-toggle-arrow">
+    <div :class="{ 'toggled': toggled }">
+      <span class="i-arrow"></span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UiToggleArrow',
+
+  props: {
+    value: Boolean
+  },
+
+  mounted () {
+    this.toggled = this.value
+  },
+
+  data: () => ({
+    toggled: false
+  }),
+
+  methods: {
+    toggle () {
+      this.toggled = !this.toggled
+      this.$emit('input', this.toggled)
+    }
+  },
+
+  watch: {
+    value (value) {
+      this.toggled = value
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .ui-toggle-arrow {
+    cursor: pointer;
+    position: relative;
+  }
+
+  .ui-toggle-arrow span {
+    transition: .3s;
+  }
+
+  .ui-toggle-arrow .toggled span {
+    transform: rotate(180deg);
+  }
+</style>
