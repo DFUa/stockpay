0e63f4f02c47a7fbb32a997510e759d9687c0bf8
Just Programmer <polishchuk256@gmail.com>
2019-02-08 15:25:59 +0200

    Add UiViewPreloader..

---
+++ src/components/ui/ui-view-preloader/UiViewPreloader.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <transition name="fade" mode="out-in">
+      <div v-if="loaded" key="slotkey">
+        <slot/>
+      </div>
+      <div v-else class="preloader-wrapper" key="preloaderkey">
+        <ui-spinner/>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+import UiSpinner from '@/components/ui/ui-spinner/UiSpinner.vue'
+
+export default {
+  name: 'UiViewPreloader',
+
+  props: {
+    loaded: Boolean
+  },
+
+  components: {
+    UiSpinner
+  }
+}
+</script>
+
+<style scoped>
+  .preloader-wrapper {
+    width: 100%;
+    height: calc(100vh - 270px);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
