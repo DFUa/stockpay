112ce1261e840f55039d545f2ea03ce73ac47e49
Just Programmer <polishchuk256@gmail.com>
2019-04-09 14:23:18 +0300

    Diasable ESLint warning

--- src/components/views/account/main/dashboard/Dashboard.vue
+++ src/components/views/account/main/dashboard/Dashboard.vue
@@ -6,16 +6,19 @@
 </template>
 
 <script>
+/* eslint-disable */
 import DashboardWallets from './DashboardWallets.vue'
 import DashboardTransactions from './DashboardTransactions.vue'
 
 export default {
   name: 'Dashboard',
+
   data () {
     return {
       devAlert: false
     }
   },
+
   beforeRouteEnter (to, from, next) {
     next (vm => {
       if (from.name && (from.name === 'auth-signin' || from.path.indexOf('auth') !== -1)) {
@@ -24,6 +27,7 @@ export default {
       next({ path: to.fullpath })
     })
   },
+
   watch: {
     devAlert (value) {
       if (value === true) {
