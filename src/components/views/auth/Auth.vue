191a533bbc27abc265fb0575481f89860f1edebc
Evgenii Khvorostyuk <yh@winstars.tech>
2019-04-19 13:13:08 +0300

    landing button on signin page

--- src/components/views/auth/Auth.vue
+++ src/components/views/auth/Auth.vue
@@ -2,6 +2,11 @@
   <div class="auth-wrapper">
     <div class="left-part"></div>
     <div class="right-part">
+      <button class="back-to-top" @click="redirectToLanding">
+        <svg width="16" height="9" viewBox="0 0 16 9" fill="none" xmlns="http://www.w3.org/2000/svg" transform="rotate(90)">
+            <path d="M7.29289 8.70711C7.68342 9.09763 8.31658 9.09763 8.70711 8.70711L15.0711 2.34315C15.4616 1.95262 15.4616 1.31946 15.0711 0.928932C14.6805 0.538408 14.0474 0.538408 13.6569 0.928932L8 6.58579L2.34315 0.928932C1.95262 0.538408 1.31946 0.538408 0.928932 0.928932C0.538408 1.31946 0.538408 1.95262 0.928932 2.34315L7.29289 8.70711ZM7 7V8H9V7H7Z" fill="black"></path>
+        </svg>
+      </button>
       <transition name="slide-right" mode="out-in">
         <router-view/>
       </transition>
@@ -11,7 +16,12 @@
 
 <script>
 export default {
-  name: 'Auth'
+  name: 'Auth',
+  methods: {
+    redirectToLanding () {
+      window.location.href = 'http://stockspay.org'
+    }
+  }
 }
 </script>
 
@@ -36,11 +46,11 @@ export default {
     min-width: 675px;
     min-height: 100vh;
     display: flex;
-    align-items: center;
     padding: 30px 90px;
   }
 
   .auth-wrapper .right-part > div {
+    align-self: center;
     width: 100%;
   }
 
@@ -101,5 +111,23 @@ export default {
     width: 100%;
     display: flex;
     justify-content: center;
+  }
+  .back-to-top {
+    padding: 7px 10px 8px;
+    background-color: rgba(0, 0, 0, .1);
+    cursor: pointer;
+    border: none;
+    border-radius: 50%;
+    outline: none;
+    -webkit-transition: all .5s;
+    -o-transition: all .5s;
+    transition: all .5s;
+    width: 64px;
+    height: 64px;
+    opacity: .75;
+    z-index: 99999;
+  }
+  .back-to-top:hover {
+    opacity: 1;
   }
 </style>
