8011971302eadf262ed4ae61bc5bc685d88bae01
Evgenii Khvorostyuk <yh@winstars.tech>
2019-04-19 14:39:23 +0300

    service pages fix

--- src/components/views/account/main/service/ServiceSidebar.vue
+++ src/components/views/account/main/service/ServiceSidebar.vue
@@ -16,13 +16,14 @@ export default {
 
   data: () => ({
     items: [
-      { id: 0, title: 'Пополнить телефон ( Для операторов России )', icon: 'i-sr-smartphone', path: 'phone' },
-      { id: 1, title: 'Пополнить ВКонтакте', icon: 'i-sr-vk', path: 'vk' },
-      { id: 2, title: 'Оплатить такси', icon: 'i-sr-frontal-taxi-cab', path: 'taxi' },
-      { id: 3, title: 'Мастер на дом', icon: 'i-sr-support', path: 'support' },
-      { id: 4, title: 'Пополнить Интернет', icon: 'i-sr-internet', path: 'internet' },
-      { id: 5, title: 'Пополнить Телевидение', icon: 'i-sr-television', path: 'television' },
-      { id: 6, title: 'Коммунальные платежи', icon: 'i-sr-house', path: 'house' },
+      { id: 0, title: 'Мастер на дом', icon: 'i-sr-support', path: 'support' },
+      { id: 1, title: 'Оплатить такси', icon: 'i-sr-frontal-taxi-cab', path: 'taxi' },
+      { id: 2, title: 'Пополнить телефон', icon: 'i-sr-smartphone', path: 'phone' },
+      // { id: 2, title: 'Пополнить телефон ( Для операторов России )', icon: 'i-sr-smartphone', path: 'phone' },
+      // { id: 1, title: 'Пополнить ВКонтакте', icon: 'i-sr-vk', path: 'vk' },
+      { id: 3, title: 'Пополнить Интернет', icon: 'i-sr-internet', path: 'internet', disabled: true },
+      { id: 4, title: 'Пополнить Телевидение', icon: 'i-sr-television', path: 'television', disabled: true },
+      { id: 5, title: 'Коммунальные платежи', icon: 'i-sr-house', path: 'house', disabled: true },
     ]
   }),
 
